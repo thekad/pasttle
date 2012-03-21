2ff3288c1cfabd9d1b85a9ea8e585000cea931b5
Jorge Gallegos <kad@blegh.net>
2012-03-21 08:47:14 -0700

    Adding comments, adding force-highlight, implementing passwod storing (no auth yet)

--- pasttle.py
+++ pasttle.py
@@ -7,9 +7,10 @@
 import bottle
 from bottle.ext import sqlalchemy as sqlaplugin
 from bottle.ext import memcache as mcplugin
+import hashlib
 import json
-import os
 import logging
+import os
 import pygments
 from pygments import formatters
 from pygments import lexers
@@ -18,12 +19,14 @@ from sqlalchemy import func
 from sqlalchemy.ext import declarative
 
 
+# Load configuration, or default
 cfg_file = os.environ.get('PASTTLECONF', 'pasttle.conf')
 CONF = json.load(open(cfg_file))
 
 debug = CONF.get('debug', False)
 format = '%(asctime)s %(levelname)s %(name)s %(message)s'
 
+# Set up logging
 LOGGER = logging.getLogger('pasttle.web')
 LOGGER.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
@@ -35,6 +38,7 @@ formatter = logging.Formatter(format)
 ch.setFormatter(formatter)
 LOGGER.addHandler(ch)
 
+# Subclass declarative base for sqla objects
 Base = declarative.declarative_base()
 
 
@@ -45,26 +49,29 @@ class Paste(Base):
     id = sqlalchemy.Column(sqlalchemy.BigInteger, primary_key=True)
     content = sqlalchemy.Column(sqlalchemy.Text, nullable=False)
     filename = sqlalchemy.Column(sqlalchemy.String(128))
-    password = sqlalchemy.Column(sqlalchemy.String(64))
+    password = sqlalchemy.Column(sqlalchemy.String(40))
     mimetype = sqlalchemy.Column(sqlalchemy.String(64), nullable=False)
-    created = sqlalchemy.Column(sqlalchemy.DateTime)
+    created = sqlalchemy.Column(sqlalchemy.DateTime, default=func.now(),
+        nullable=False)
 
     def __init__(self, content, mimetype, filename=None, password=None):
         self.content = content
         self.mimetype = mimetype
-        if filename:
+        if filename and filename.strip():
             self.filename = filename.strip()[:128]
-        self.password = password
-        self.created = func.now()
+        self.password = hashlib.sha1(password).hexdigest()
 
     def __repr__(self):
-        return u'<Paste "%s" (%s), locked=%s>' % (self.filename,
+        return u'<Paste "%s" (%s), protected=%s>' % (self.filename,
             self.mimetype, bool(self.password))
 
 app = bottle.Bottle()
 engine = sqlalchemy.create_engine(CONF['dsn'], echo=debug,
     convert_unicode=True, logging_name='pasttle.db')
+# Create all metadata on loading, if something blows we need to know asap
 Base.metadata.create_all(engine)
+
+# Install sqlalchemy and memcache plugins
 db_plugin = sqlaplugin.SQLAlchemyPlugin(engine, Base.metadata, create=True)
 app.install(db_plugin)
 mc_plugin = mcplugin.MemcachePlugin(servers=CONF.get('memcache',
@@ -83,6 +90,10 @@ NAME
 
 SYNOPSIS
     &lt;command&gt; | curl -F 'upload=@-' %(url)s
+
+    curl -F 'upload=@filename.ext' %(url)s
+
+    curl -F 'upload=@filename.ext' -F 'password=humptydumpty' %(url)s
 </pre>
     """ % {
         'url': bottle.request.url,
@@ -126,7 +137,7 @@ def post(db):
             mime = u'text/plain'
         paste = Paste(content=raw, mimetype=mime,
             filename=filename, password=password)
-        logging.debug(paste)
+        LOGGER.debug(paste)
         db.add(paste)
         db.commit()
         (scheme, host, path, qs, fragment) = bottle.request.urlparts
@@ -139,10 +150,17 @@ def _get_paste(db, id):
     paste = db.query(Paste).filter_by(id=id).one()
     return paste
 
+
 @app.route('/<id:int>')
-def showpaste(db, id):
+def showpaste(db, id, lang=None):
     paste = _get_paste(db, id)
-    lexer = lexers.get_lexer_for_mimetype(paste.mimetype)
+    if lang:
+        try:
+            lexer = lexers.get_lexer_by_name(lang)
+        except lexers.ClassNotFound:
+            lexer = lexers.get_lexer_by_name('text')
+    else:
+        lexer = lexers.get_lexer_for_mimetype(paste.mimetype)
     title = u'%s (%s), created on %s' % (paste.filename or u'',
         paste.mimetype, paste.created, )
     LOGGER.debug(lexer)
@@ -151,8 +169,14 @@ def showpaste(db, id):
         formatters.HtmlFormatter(full=True, linenos='table',
             encoding='utf-8', lineanchors='ln', title=title))
 
-@app.route('/raw/<id:int>')
-def showraw(db, mc, id):
+
+@app.route('/<id:int>/<lang>')
+def forcehighlight(db, id, lang):
+    return showpaste(db, id, lang)
+
+
+@app.route('/raw/<id:int>', apply=[db_plugin, mc_plugin])
+def showraw(db, id):
     paste = _get_paste(db, id)
     bottle.response.content_type = 'text/plain'
     return paste.content
