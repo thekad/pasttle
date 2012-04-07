9595a636ddbdd0df13afc59c5d92bce2a798f831
Jorge Gallegos <kad@blegh.net>
2012-04-06 22:23:45 -0700

    Adding a simple setup script

---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+#
+# -*- mode:python; sh-basic-offset:4; indent-tabs-mode:nil; coding:utf-8 -*-
+# vim:set tabstop=4 softtabstop=4 expandtab shiftwidth=4 fileencoding=utf-8:
+#
+
+import sys
+from setuptools import setup
+
+extra = {
+    'install_requires': [
+        'distribute',
+        'bottle>=0.9',
+        'Pygments',
+        'SQLAlchemy',
+        'bottle-sqlalchemy',
+        'bottle-sqlite',
+    ]
+}
+
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+
+setup(
+    name = 'pasttle',
+    version = '0.4',
+    url = 'http://github.com/thekad/pasttle/',
+    description = 'Simple pastebin on top of bottle.',
+    author = 'Jorge Gallegos',
+    author_email = 'kad@blegh.net',
+    license = 'MIT',
+    platforms = 'any',
+    zip_safe = False,
+    scripts = [
+        'pasttle.py',
+    ],
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    **extra
+)
+
