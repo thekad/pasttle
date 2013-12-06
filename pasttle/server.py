ee475c27bdcf61ee25c1f726ae08e9b3c4b47fc2
Jorge Gallegos <kad@blegh.net>
2013-12-06 14:38:04 -0800

    Sure, why not

--- pasttle/server.py
+++ pasttle/server.py
@@ -316,6 +316,21 @@ def _password_protect_form():
 
     return u"""<html>
     <head>
+        <style>
+            body {
+                font-family: Courier;
+                font-size: 12px;
+            }
+
+            p {
+                margin: 20px;
+            }
+
+            a {
+                text-decoration: none;
+                font-weight: bold;
+            }
+        </style>
     </head>
     <body>
         <p>This entry is password protected, write in your password:</p>
