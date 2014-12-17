54dcb5acb5cceee4548423704660fc15724c8258
Jorge Gallegos <kad@blegh.net>
2014-12-17 07:46:49 -0800

    Oh it seems Pygments doesn't support py3.2

--- setup.py
+++ setup.py
@@ -49,7 +49,6 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Environment :: Web Environment',
