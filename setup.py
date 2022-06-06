61c3cb99fe5f974bc0209a60bc24e340706a45ad
Jorge Gallegos <kad@blegh.net>
2022-06-06 16:31:52 -0500

    Updating classifiers for python 3.7+

--- setup.py
+++ setup.py
@@ -41,10 +41,10 @@ setuptools.setup(
         ],
     },
     classifiers=[
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Development Status :: 4 - Beta',
         'Environment :: Console',
         'Environment :: Web Environment',
